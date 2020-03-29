91063c77b9a18f34e42c1c566c28cb7af39ae54f
Sharky <charlton.rose@alianza.com>
2020-03-29 10:25:49 -0600

    Generate product-details component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
@@ -19,6 +20,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
   declarations: [
     AppComponent,
     ProductAlertsComponent,
+    ProductDetailsComponent,
     ProductListComponent,
     TopBarComponent,
   ],
