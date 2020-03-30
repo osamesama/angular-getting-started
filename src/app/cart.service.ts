8b7cfa5c8b32d85d7b2baf646c14c098c23940b0
Sharky <charlton.rose@alianza.com>
2020-03-30 17:10:41 -0600

    Define cart service.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { Product } from './products';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  items: Product[] = [];
+
   constructor() { }
+
+  addToCart(product: Product): void {
+    this.items.push(product);
+  }
+
+  getItems(): Product[] {
+    return this.items;
+  }
+
+  clearCart(): void {
+    this.items = [];
+  }
 
 }
