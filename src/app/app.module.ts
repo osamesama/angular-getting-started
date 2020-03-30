de80d45748be290bbba9709f5fa78ebeb0e49750
Sharky <charlton.rose@alianza.com>
2020-03-30 17:39:46 -0600

    Generate cart component.
    Command: ng generate component cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
@@ -20,6 +21,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
   ],
   declarations: [
     AppComponent,
+    CartComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
     ProductListComponent,
